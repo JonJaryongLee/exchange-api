462f9373e47429d05142f9f2e624625e466f6dab
JonJaryongLee <director9303@gmail.com>
2022-12-28 11:14:49 +0900

    221228

---
+++ CalcView.vue
@@ -0,0 +1,133 @@
+<template>
+  <div>
+    <h1>환율을 알아보자!</h1>
+    <ul>
+      <li v-for="exchange in exchanges" v-bind:key="exchange.id">
+        <div>통화코드: {{ exchange.cur_unit }}</div>
+        <div>국가/통화명: {{ exchange.cur_nm }}</div>
+        <div>살 때: {{ exchange.ttb }}원</div>
+        <div>팔 때: {{ exchange.tts }}원</div>
+        <hr />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+// 배열 정렬 위해 lodash 설치
+import _ from "lodash";
+// 날짜 라이브러리 dayjs
+import dayjs from "dayjs";
+export default {
+  data() {
+    return {
+      exchanges: [],
+    };
+  },
+  created() {
+    const now = dayjs("2022-12-25");
+    this.getExchangeData(now);
+  },
+  methods: {
+    parseExchanges(exchanges) {
+      const keyCountries = [
+        {
+          id: 1,
+          cur_unit: "USD",
+        },
+        {
+          id: 2,
+          cur_unit: "EUR",
+        },
+        {
+          id: 3,
+          cur_unit: "JPY(100)",
+        },
+        {
+          id: 4,
+          cur_unit: "CNH",
+        },
+        {
+          id: 5,
+          cur_unit: "GBP",
+        },
+        // 원화는 항상 1이 나오므로, 아무 의미없어서 맨 마지막에 둠
+        {
+          id: 999,
+          cur_unit: "KRW",
+        },
+      ];
+      // 기축통화국 이외 나머지 국가는 6번부터 부여
+      let cnt = 6;
+      this.exchanges = exchanges.map((exchange) => {
+        // 우선, 각각의 엘리먼트에 id 와 key_country 를 null 로 부여
+        const format = {
+          id: null,
+          key_country: null,
+          cur_unit: exchange.cur_unit,
+          cur_nm: exchange.cur_nm,
+          ttb: exchange.ttb,
+          tts: exchange.tts,
+        };
+        // 현재 exchange 가 key_country 인지 확인하고,
+        // 맞으면 길이 1의 배열로 해당 국가 뽑아내고
+        // 아니라면 길이 0의 빈 배열 리턴
+        const targetElement = keyCountries.filter((keyCountry) => {
+          return keyCountry.cur_unit === exchange.cur_unit;
+        });
+        // 길이가 있다는 건 key_country 라는 뜻
+        if (targetElement.length) {
+          format.key_country = true;
+          // 해당 주요국의 아이디 부여. 대한민국은 999
+          format.id = targetElement[0].id;
+          return format;
+        } else {
+          format.key_country = false;
+          // 주요국이 아니라면 6번부터 부여
+          format.id = cnt;
+          cnt = cnt + 1;
+          return format;
+        }
+      });
+      // 아이디 기준 배열 정렬함
+      this.exchanges = _.sortBy(this.exchanges, "id");
+      // 대한민국은 맨 마지막 999번이고, 특별히 도움되는 정보가 없으므로 제외
+      this.exchanges.pop();
+    },
+    // 이 함수는 재귀함수다.
+    async getExchangeData(date) {
+      const parsedDate = date.format("YYYY-MM-DD");
+      // 해당 API 사용 시, 프록시서버를 두어야만 CORS 해결됨
+      // 개발 과정에선 단순히 크롬 CORS 익스텐션 설치해서 테스트함
+      const URL =
+        "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON";
+      const params = {
+        authkey: "API 키를 입력하시오",
+        data: "AP01",
+        searchdate: parsedDate,
+      };
+      try {
+        const response = await axios.get(URL, {
+          params: params,
+        });
+        if (response.data) {
+          // 주말이나 공휴일, 평일 오전 10시 이전은 환율 정보가 빈 배열 [] 로 옴
+          // 따라서, getExchangeData 를 재귀로 작동시켜, 유효한 배열이 올때까지 시도
+          // 무한루프 주의. API 일일 횟수제한이 있으니 테스트 시 setTimeout 사용 추천
+          if (response.data.length === 0) {
+            // dayjs 에서 날짜 하루 뺄 때는 subtract 사용
+            // 즉, 빈 배열 [] 이면 하루씩 빼가면서 계속 시도
+            this.getExchangeData(date.subtract(1, "day"));
+          } else {
+            // 유효한 배열을 받았을 시, 파싱함수 작동시켜 패치
+            this.parseExchanges(response.data);
+          }
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
---
+++ README.md
@@ -0,0 +1,20 @@
+# 환율 API 활용
+
+Created: 2022년 12월 28일 오전 10:53
+Last Edited Time: 2022년 12월 28일 오전 11:05
+
+오늘 해볼 것은 환율 API 활용이다. 
+
+이 예제 사용 시 다음과 같은 장점이 있다.
+
+- 공공 API 사용법 익히기
+- npm 라이브러리 적용 (dayjs, lodash)
+- 재귀함수 사용
+- JS 파싱능력 향상
+- 클라이언트 CORS 임시 해결법: 크롬 CORS 익스텐션 사용
+
+사용한 공공 API: 한국수출입은행 환율정보
+
+[https://www.koreaexim.go.kr/ir/HPHKIR020M01?apino=2&viewtype=C&searchselect=&searchword=](https://www.koreaexim.go.kr/ir/HPHKIR020M01?apino=2&viewtype=C&searchselect=&searchword=)
+
+![Untitled](markdown-img/Untitled.png)
